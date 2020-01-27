8126e3557405112dee41556491f38665b42aa241
Bartłomiej Sienko <bartek_sk@interia.pl>
2020-01-27 17:51:15 +0100

    letter case file name fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from '../layout/Layout'
-import AppWrapper from '../Layout/AppWrapper'
+import AppWrapper from '../layout/AppWrapper'
 
 export default () => (
     <Layout>
