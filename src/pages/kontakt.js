4251048ecb4af6fce952d4e08628e9f7c2ff23e5
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-29 22:24:15 +0100

    1

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import Layout from '../layout/Layout'
+import Contact from '../components/section_components/Contact'
 
 const kontakt = () => {
     return (
         <Layout>
-
+            <Contact />
         </Layout>
     )
 }
