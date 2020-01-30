196710a8e63d9350598b2816f789804e57ee5ca5
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-30 20:04:47 +0100

    2

--- src/layout/AppWrapper.js
+++ src/layout/AppWrapper.js
@@ -1,50 +1,50 @@
-import React from 'react'
-// import AppContext from '../context/AppContext'
-import styled from 'styled-components'
+// import React from 'react'
+// // import AppContext from '../context/AppContext'
+// import styled from 'styled-components'
 
-// import Nav from '../components/section_components/Nav'
-import Hero from '../components/section_components/Hero'
-import About from '../components/section_components/About'
-import Gallery from '../components/section_components/Gallery'
-import Contact from '../components/section_components/Contact'
-// import SideMenu from '../components/section_components/SideMenu'
+// // import Nav from '../components/section_components/Nav'
+// import Hero from '../components/section_components/Hero'
+// import About from '../components/section_components/About'
+// import Gallery from '../components/section_components/Gallery'
+// import Contact from '../components/section_components/Contact'
+// // import SideMenu from '../components/section_components/SideMenu'
 
-const Wrapper = styled.div`
+// const Wrapper = styled.div`
 
-`
+// `
 
-const SectionsWrapper = styled.div`
-    transition: 1s ${props => props.theme.transitions.cubic_front};
-    &.blurred{
-        transform: translateX(35px);
-        opacity: .3;
-    }
-    @media (max-width: 750px) {
-        &.blurred{
-        transform: translateY(-35px);
-        opacity: .3;
-    }
-    }
+// const SectionsWrapper = styled.div`
+//     transition: 1s ${props => props.theme.transitions.cubic_front};
+//     &.blurred{
+//         transform: translateX(35px);
+//         opacity: .3;
+//     }
+//     @media (max-width: 750px) {
+//         &.blurred{
+//         transform: translateY(-35px);
+//         opacity: .3;
+//     }
+//     }
 
-`
+// `
 
 
-const AppWrapper = () => {
+// const AppWrapper = () => {
 
-    // const { isNavOpen } = useContext(AppContext)
+//     // const { isNavOpen } = useContext(AppContext)
 
-    return (
-        <Wrapper>
-            {/* <SideMenu />
-            <Nav /> */}
-            <SectionsWrapper>
-                <Hero />
-                <About />
-                <Gallery />
-                <Contact />
-            </SectionsWrapper>
-        </Wrapper>
-    )
-}
+//     return (
+//         <Wrapper>
+//             {/* <SideMenu />
+//             <Nav /> */}
+//             <SectionsWrapper>
+//                 <Hero />
+//                 <About />
+//                 <Gallery />
+//                 <Contact />
+//             </SectionsWrapper>
+//         </Wrapper>
+//     )
+// }
 
-export default AppWrapper
+// export default AppWrapper
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,16 @@ import AppWrapper from '../layout/AppWrapper'
 
 import Layout from '../layout/Layout'
 
+import Hero from '../components/section_components/Hero'
+import About from '../components/section_components/About'
+import Gallery from '../components/section_components/Gallery'
+import Contact from '../components/section_components/Contact'
+
 export default () => (
     <>
-        <AppWrapper />
+        <Hero />
+        <About />
+        <Gallery />
+        <Contact />
     </>
 )
