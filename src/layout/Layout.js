48c2210ee9ad71bf405a0cd02bf6922e404c3d5d
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-31 06:29:11 +0100

    vibrate time 100ms

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
     // functions
     const handleNavOpen = () => {
         setIsNavOpen(!isNavOpen)
-        window.navigator.vibrate(200);
+        window.navigator.vibrate(100);
     }
 
     return (
