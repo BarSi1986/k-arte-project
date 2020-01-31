5b15558e28ab7809980d7d5b0bc07ff74706c634
Bartłomiej Sienko <bartlomiejsienko@MacBook-Air-Bartomiej.local>
2020-01-31 06:31:21 +0100

    50ms

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
     // functions
     const handleNavOpen = () => {
         setIsNavOpen(!isNavOpen)
-        window.navigator.vibrate(100);
+        window.navigator.vibrate(50);
     }
 
     return (
